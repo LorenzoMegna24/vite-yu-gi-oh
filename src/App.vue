d7f3a59344f784faedfd53e8784c7196e01f37d9
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-05 16:32:08 +0200

    Imorto API

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <script>
+  import axios from 'axios';
+  import {store} from './store'
   import HeaderComp from './components/HeaderComp.vue';
 
   export default{
-    name: App,
+    name: 'App',
     components:{
       HeaderComp,
     },
+    data(){
+      return{
+        store
+      }
+    },
+    created(){
+      this.callApi()
+    },
+    methods:{
+      callApi(){
+        axios.get( 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=30&offset=3' ).then((res)=>{
+          console.log(res.data);
+
+          const datiApi = res.data
+
+          this.store.arrayCarte = datiApi
+        })
+      }
+    }
   }
 </script>
 
 <template>
  <HeaderComp/>
+ <main class="container">
+  
+ </main>
 </template>
 
 <style lang="scss" scoped>
-@use './style/main.scss'
+@use './style/main.scss';
 </style>
