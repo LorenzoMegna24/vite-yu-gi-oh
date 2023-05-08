371e6550476fffa8b9c29c935440f67dedfce549
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-08 17:10:41 +0200

    Fix css

--- src/components/SearchComp.vue
+++ src/components/SearchComp.vue
@@ -14,13 +14,14 @@ export default{
 </script>
 
 <template>
-  <div>
-    <select name="cards" id="cards" v-model="store.RicercaCarte">Type
-    <option
-    v-for="(element, index) in store.arrayType"
-    :key="index"
-    @click.prevent="$emit('nomeEmit')"
-    :value="element.archetype_name">{{ element.archetype_name }}</option>
+  <div class="d-flex justify-content-center">
+    <select class="form-select w-50" aria-label="Default select example" name="cards" id="cards" v-model="store.RicercaCarte">
+      <option selected="">Open this select menu</option>
+      <option
+      v-for="(element, index) in store.arrayType"
+      :key="index"
+      @click.prevent="$emit('nomeEmit')"
+      :value="element.archetype_name">{{ element.archetype_name }}</option>
   </select>
   </div>
 </template>
--- src/components/SingolaCarta.vue
+++ src/components/SingolaCarta.vue
@@ -7,10 +7,14 @@ export default {
 </script>
 
 <template>
-    <div class="carta col-12 col-md-6 col-lg-4">
-      <img :src="dettagliCarte.card_images[0].image_url" alt="">
-      <h4>{{ dettagliCarte.name }}</h4>
-      <span>{{ dettagliCarte.archetype }}</span>
+    <div class="carta col-12 col-md-6 col-lg-3">
+      <div class="card" style="width: 18rem;">
+       <img class="p-1" :src="dettagliCarte.card_images[0].image_url" alt="">
+        <div class="card-body">
+         <h5 class="card-title">{{ dettagliCarte.name }}</h5>
+         <p class="card-text">{{ dettagliCarte.archetype }}</p>
+        </div>
+      </div>
     </div>
 </template>
 
